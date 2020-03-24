a9c60994fa50b4522929a864544d61590ed135d9
yuchan <lumir1031@gmail.com>
2020-03-24 13:52:40 +0800

    update

--- 0320-ver2-hw.rb
+++ 0320-ver2-hw.rb
@@ -2,20 +2,20 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 class ATM
-    attr_reader :balance
-    def initialize(balance)
-       @balance = balance
-     end
-     def deposit(amount)
-       if amount >= 0
-         @balance += amount
-       end
-     end
-     def withdraw(amount)
-       if amount > 0 && amount < @balance
-         @balance -= amount
-       end
-     end
+  attr_reader :balance
+  def initialize(balance)
+     @balance = balance
+  end
+  def deposit(amount)
+    if amount >= 0
+      @balance += amount
+    end
+  end
+  def withdraw(amount)
+    if amount > 0 && amount < @balance
+      @balance -= amount
+    end
+  end
 end
 
 get '/' do
@@ -24,16 +24,16 @@ end
 
 post '/atm' do
 
-    choice = params[:choice]
-    amount = params[:amount].to_i
+  behavior = params[:behavior]
+  amount = params[:amount].to_i
   
     
-    atm = ATM.new(0)
-    if choice == "plus" 
-      atm.deposit(amount)
-    else choice == "minus"
-      atm.withdraw(amount)
-    end  
+  atm = ATM.new(0)
+  if behavior == "+" 
+    atm.deposit(amount)
+  else behavior == "-"
+    atm.withdraw(amount)
+  end  
     
 
 
